f7cbe590fc149f5ffa90374d5f4154b0dd51ef3c
Lorena <lorena.perez@gmail.com>
2021-06-08 09:51:35 +0200

    add keys

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 const Pokemon = (props) => {
-  const pokemon = props.pokemon.types.map((type) => {
-    return <div>{type}</div>;
+  const pokemon = props.pokemon.types.map((type, index) => {
+    return <li key={index}>{type}</li>;
   });
   return (
     <article className="pokemon">
       <img src={props.pokemon.url} alt="" />
       <h2 className="pokemon__name">{props.pokemon.name}</h2>
-      <div>{pokemon}</div>
+      <ul>{pokemon}</ul>
     </article>
   );
 };
